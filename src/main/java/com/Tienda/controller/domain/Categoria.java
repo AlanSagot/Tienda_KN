aa0b91d156e1658c40b5b6952d257e1776cd9e50
Alan Sagot <alansagotcespedes@gmail.com>
2023-10-13 11:40:43 -0600

    Semana 5 (completo)

---
+++ src/main/java/com/Tienda/controller/domain/Categoria.java
@@ -0,0 +1,39 @@
+package com.Tienda.controller.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data // PARA LA CREACION AUTOMATICA DE LOS SET AND GET DE LOS ATRIBUTOS
+@Entity //HACE QUE ESTA CLASE SEA UNA ENTIDAD
+@Table(name = "categoria")
+public class Categoria implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long idCategoria; //Lo transforma en id_Categoria
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Categoria(String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+
+    public boolean isActivo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
